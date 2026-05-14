c0994d85717070e52f8d7641e4c1d46199c68746
Puyol <Puyol@LAPTOP-OINMS7KJ>
2026-05-14 12:06:48 +0900

    a

--- src/main/java/scoremanager/main/TestRegistAction.java
+++ src/main/java/scoremanager/main/TestRegistAction.java
@@ -6,33 +6,16 @@ import tool.Action;
 
 public class TestRegistAction extends Action {
 
-    @Override
-    public void execute(
-        HttpServletRequest req,
-        HttpServletResponse res
-    ) throws Exception {
+	@Override
+	public void execute(
+		HttpServletRequest req,
+		HttpServletResponse res
+	) throws Exception {
 
-        // リクエストパラメータ取得
-        String studentNo =
-            req.getParameter("studentNo");
+		String url =
+			"/scoremanager/main/test_regist.jsp";
 
-        String subjectCd =
-            req.getParameter("subjectCd");
-
-        String point =
-            req.getParameter("point");
-
-        // 確認用
-        System.out.println(studentNo);
-        System.out.println(subjectCd);
-        System.out.println(point);
-
-        // 遷移先
-        String url =
-            "/scoremanager/main/test_regist.jsp";
-
-        // フォワード
-        req.getRequestDispatcher(url)
-            .forward(req, res);
-    }
+		req.getRequestDispatcher(url)
+			.forward(req, res);
+	}
 }
---
+++ src/main/java/scoremanager/main/TestRegistExecuteAction.java
@@ -0,0 +1,37 @@
+package scoremanager.main;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tool.Action;
+
+public class TestRegistExecuteAction extends Action {
+
+	@Override
+	public void execute(
+		HttpServletRequest req,
+		HttpServletResponse res
+	) throws Exception {
+
+		// リクエストパラメータ取得
+		String studentNo =
+			req.getParameter("studentNo");
+
+		String subjectCd =
+			req.getParameter("subjectCd");
+
+		String point =
+			req.getParameter("point");
+
+		// 確認用
+		System.out.println(studentNo);
+		System.out.println(subjectCd);
+		System.out.println(point);
+
+		// 完了画面へ
+		String url =
+			"/scoremanager/main/test_regist_done.jsp";
+
+		req.getRequestDispatcher(url)
+			.forward(req, res);
+	}
+}
